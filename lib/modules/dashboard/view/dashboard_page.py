11b244b9ce2d34627b4a51c599f176af5122b8af
JuanFcoFernandez <juaffern@espol.edu.ec>
2024-08-22 03:01:40 -0500

    dashboard_page updated [ADD]

--- lib/modules/dashboard/view/dashboard_page.py
+++ lib/modules/dashboard/view/dashboard_page.py
@@ -1,12 +1,47 @@
+from typing import Any, Tuple
+
+import customtkinter as ctk
+
 from packages.models import User
 from packages.task_repository import TaskRepository
 
 from ..bloc.dashboard_bloc import DashboardBloc
+from ..bloc.dashboard_events import DashboardSubscriptionRequested
 
 
-class DashboardPage:
-    def __init__(self, *, taskRepository: TaskRepository, user: User) -> None:
+class DashboardPage(ctk.CTkFrame):
+    def __init__(
+        self,
+        master: Any,
+        width: int = 200,
+        height: int = 200,
+        corner_radius: int | str | None = None,
+        border_width: int | str | None = None,
+        bg_color: str | Tuple[str, str] = "transparent",
+        fg_color: str | Tuple[str, str] | None = None,
+        border_color: str | Tuple[str, str] | None = None,
+        background_corner_colors: Tuple[str | Tuple[str, str]] | None = None,
+        overwrite_preferred_drawing_method: str | None = None,
+        *,
+        taskRepository: TaskRepository,
+        user: User,
+        **kwargs
+    ):
+        super().__init__(
+            master,
+            width,
+            height,
+            corner_radius,
+            border_width,
+            bg_color,
+            fg_color,
+            border_color,
+            background_corner_colors,
+            overwrite_preferred_drawing_method,
+            **kwargs
+        )
         self.dashboardBloc = DashboardBloc(taskRepository=taskRepository, user=user)
+        self.dashboardBloc.add(event=DashboardSubscriptionRequested())
 
     def build(self):
-        num1 = self.dashboardBloc.state.completedTasks
+        num1 = len(self.dashboardBloc.state.completedTasks)
